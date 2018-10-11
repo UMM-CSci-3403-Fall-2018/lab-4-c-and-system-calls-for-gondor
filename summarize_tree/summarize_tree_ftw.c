75ca774dbf3ff98290ab76744cb83194b85c19d9
stev0531 <stev0531@morris.umn.edu>
2018-10-11 11:33:16 -0500

    Add conditional statements in callback
    These statements check if an item is a
    directory or a file and increments the
    corresponding variables. Included
    print statements in main. All tests passing
    in `summarize_tree_ftw.c`.

--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <ftw.h>
 
 static int num_dirs, num_regular;
 
 static int callback(const char *fpath, const struct stat *sb, int typeflag) { 
-    // Define stuff here 
+  // Define stuff here
+  if(typeflag == FTW_D){
+    num_dirs++;
+  } else {
+    num_regular++;
+  }
+
+  return 0;
 }
 
 #define MAX_FTW_DEPTH 16
 
 int main(int argc, char** argv) { 
-    // Check arguments and set things up
+  // Check arguments and set things up
   if (argc != 2) {
     printf ("Usage: %s <path>\n", argv[0]);
     printf ("       where <path> is the file or root of the tree you want to summarize.\n");
@@ -20,7 +28,11 @@ int main(int argc, char** argv) {
   num_regular = 0;
 
 
-    ftw(argv[1], callback, MAX_FTW_DEPTH);
+  ftw(argv[1], callback, MAX_FTW_DEPTH);
 
-    // Print out the results 
+  // Print out the results 
+  printf("There were %d directories.\n", num_dirs);
+  printf("There were %d regular files.\n", num_regular);
+
+  return 0;
 }
